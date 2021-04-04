6141e764997fc9b5888690ccc04908aafc33eeb3
Mark Toda <toda.mark@gmail.com>
2021-04-03 20:33:52 -0700

    Remove orchestrator
    This commit removes the orchestrator contract and associated contracts
    / files as it is not necessary for the button token protocol

--- contracts/interfaces/IOrchestrator.sol
+++
@@ -1,6 +0,0 @@
-// pragma solidity ^0.4.24;
-
-// Public interface definition for the Ampleforth Orchestrator on Ethereum (the base-chain)
-interface IOrchestrator {
-    function rebase() external;
-}

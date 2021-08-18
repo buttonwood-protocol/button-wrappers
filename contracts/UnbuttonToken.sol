d78cda294ee5f7413361d088e7dd229caafeb207
nithin <nithin@fragments.org>
2021-08-18 13:41:57 -0400

    made math functions pure

--- contracts/UnbuttonToken.sol
+++ contracts/UnbuttonToken.sol
@@ -27,7 +27,7 @@ contract UnbuttonToken is Initializable, IUnbuttonToken, ERC20 {
     ///      the `totalUnderlying` balance is always non-zero.
     uint256 public constant MINIMUM_DEPOSIT = 1_000;
 
-    // TODO: recauclate this
+    // TODO: recalculate this
     /// @dev The maximum units of the underlying token that can be
     ///      safely deposited into this contract without any numeric overflow.
     /// MAX_UNDERLYING = sqrt(MAX_UINT256/INITIAL_RATE)
@@ -69,17 +69,19 @@ contract UnbuttonToken is Initializable, IUnbuttonToken, ERC20 {
             "UnbuttonToken: too many unbutton tokens to mint"
         );
 
-        if (totalSupply() == 0) {
+        uint256 totalSupply_ = totalSupply();
+
+        if (totalSupply_ == 0) {
             IERC20(underlying).safeTransferFrom(msg.sender, address(this), MINIMUM_DEPOSIT);
 
-            _mint(address(this), _fromUnderlyingAmount(MINIMUM_DEPOSIT, totalUnderlying_));
+            _mint(address(this), _fromUnderlyingAmount(MINIMUM_DEPOSIT, totalUnderlying_, totalSupply_));
 
             totalUnderlying_ = _queryUnderlyingBalance();
 
             uAmount -= MINIMUM_DEPOSIT;
         }
 
-        uint256 mintAmount = _fromUnderlyingAmount(uAmount, totalUnderlying_);
+        uint256 mintAmount = _fromUnderlyingAmount(uAmount, totalUnderlying_, totalSupply_);
 
         require(mintAmount > 0, "UnbuttonToken: too few unbutton tokens to mint");
 
@@ -92,7 +94,7 @@ contract UnbuttonToken is Initializable, IUnbuttonToken, ERC20 {
 
     /// @inheritdoc IButtonWrapper
     function withdraw(uint256 uAmount) external override returns (uint256) {
-        uint256 burnAmount = _fromUnderlyingAmount(uAmount, _queryUnderlyingBalance());
+        uint256 burnAmount = _fromUnderlyingAmount(uAmount, _queryUnderlyingBalance(), totalSupply());
 
         require(burnAmount > 0, "UnbuttonToken: too few unbutton tokens to burn");
 
@@ -106,8 +108,9 @@ contract UnbuttonToken is Initializable, IUnbuttonToken, ERC20 {
     /// @inheritdoc IButtonWrapper
     function withdrawAll() external override returns (uint256) {
         uint256 totalUnderlying_ = _queryUnderlyingBalance();
-        uint256 uAmount = _toUnderlyingAmount(balanceOf(msg.sender), totalUnderlying_);
-        uint256 burnAmount = _fromUnderlyingAmount(uAmount, totalUnderlying_);
+        uint256 totalSupply_ = totalSupply();
+        uint256 uAmount = _toUnderlyingAmount(balanceOf(msg.sender), totalUnderlying_, totalSupply_);
+        uint256 burnAmount = _fromUnderlyingAmount(uAmount, totalUnderlying_, totalSupply_);
 
         require(burnAmount > 0, "UnbuttonToken: too few unbutton tokens to burn");
 
@@ -128,36 +131,35 @@ contract UnbuttonToken is Initializable, IUnbuttonToken, ERC20 {
 
     /// @inheritdoc IButtonWrapper
     function balanceOfUnderlying(address owner) external view override returns (uint256) {
-        return _toUnderlyingAmount(balanceOf(owner), _queryUnderlyingBalance());
+        return _toUnderlyingAmount(balanceOf(owner), _queryUnderlyingBalance(), totalSupply());
     }
 
     //--------------------------------------------------------------------------
     // Private methods
 
+    /// @dev Queries the underlying ERC-20 balance of this contract.
+    function _queryUnderlyingBalance() private view returns (uint256) {
+        return IERC20(underlying).balanceOf(address(this));
+    }
+
     /// @dev Converts underlying to unbutton token amount.
-    function _fromUnderlyingAmount(uint256 uAmount, uint256 totalUnderlying_)
+    function _fromUnderlyingAmount(uint256 uAmount, uint256 totalUnderlying_, uint256 totalSupply)
         private
-        view
+        pure
         returns (uint256)
     {
         return
             (totalUnderlying_ > 0)
-                ? (uAmount * totalSupply()) / totalUnderlying_
+                ? (uAmount * totalSupply) / totalUnderlying_
                 : uAmount * INITIAL_RATE;
     }
 
     /// @dev Converts unbutton to underlying token amount.
-    function _toUnderlyingAmount(uint256 amount, uint256 totalUnderlying_)
+    function _toUnderlyingAmount(uint256 amount, uint256 totalUnderlying_, uint256 totalSupply)
         private
-        view
+        pure
         returns (uint256)
     {
-        uint256 totalSupply = totalSupply();
         return (totalSupply > 0) ? (amount * totalUnderlying_) / totalSupply : 0;
-    }
-
-    /// @dev Queries the underlying ERC-20 balance of this contract.
-    function _queryUnderlyingBalance() private view returns (uint256) {
-        return IERC20(underlying).balanceOf(address(this));
     }
 }

645320e843a96ed40c2371951a690e64a6602c1f
Mark Toda <toda.mark@gmail.com>
2021-10-15 15:42:53 -0700

    feat: add deploy script for button token
    This commit adds a deploy script for button token. It is dependent on
    the ButtonTokenFactory implementatino so I made some assumptions that
    may need to be fixed up once that feature is in

---
+++ tasks/deployers/button.ts
@@ -0,0 +1,101 @@
+import { Signer } from '@ethersproject/abstract-signer'
+import { task, types } from 'hardhat/config'
+import { TaskArguments } from 'hardhat/types'
+
+task('deploy:ButtonTokenFactory')
+  .setAction(async function (
+  _args: TaskArguments,
+  hre,
+) {
+  console.log('Signer', await (await hre.ethers.getSigners())[0].getAddress());
+  const TokenTemplate = await hre.ethers.getContractFactory('ButtonToken')
+  const tokenTemplate = await TokenTemplate.deploy()
+  await tokenTemplate.deployed()
+  const templateAddress = tokenTemplate.address
+  console.log(`ButtonToken template deployed to ${templateAddress}`)
+
+  const Factory = await hre.ethers.getContractFactory('ButtonTokenFactory')
+  const factory = await Factory.deploy(templateAddress)
+  await factory.deployed()
+  const factoryAddress = factory.address
+  console.log(`ButtonTokenFactory deployed to ${factoryAddress}`)
+
+  try {
+    await hre.run('verify:ButtonToken', { address: templateAddress })
+  } catch (e) {
+    console.log('Unable to verify on etherscan', e)
+  }
+
+  try {
+    await hre.run('verify:ButtonTokenFactory', {
+      address: factoryAddress,
+      template: templateAddress,
+    })
+  } catch (e) {
+    console.log('Unable to verify on etherscan', e)
+  }
+})
+
+task('verify:ButtonToken', 'Verifies on etherscan')
+  .addParam('address', 'the contract address', undefined, types.string, false)
+  .setAction(async function (args: TaskArguments, hre) {
+    const { address } = args
+
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments: [],
+    })
+  })
+
+task('verify:ButtonTokenFactory', 'Verifies on etherscan')
+  .addParam('address', 'the contract address', undefined, types.string, false)
+  .addParam('template', 'the template address', undefined, types.string, false)
+  .setAction(async function (args: TaskArguments, hre) {
+    const { address, template } = args
+
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments: [template],
+    })
+  })
+
+task('deploy:ButtonToken')
+  .addParam('factory', 'the reference to the button token factory')
+  .addParam('underlying', 'the underlying token')
+  .addParam('name', 'the name of the button token')
+  .addParam('symbol', 'the symbol of button token')
+  .addParam('oracle', 'the oracle address. deploy with deploy:ChainlinkOracle')
+  .setAction(async function (args: TaskArguments, hre) {
+    const { factory, underlying, name, symbol, oracle } = args
+    const accounts: Signer[] = await hre.ethers.getSigners()
+    const deployer = accounts[0]
+
+    const bFactory = await hre.ethers.getContractAt(
+      'ButtonTokenFactory',
+      factory,
+    )
+
+    const deployedAddress = await bFactory.callStatic['create(address,string,string,address)'](underlying, name, symbol, oracle)
+    const tx = await bFactory['create(address,string,string,address)'](underlying, name, symbol, oracle)
+    await tx.wait()
+    console.log('Successfully created tx', tx.hash)
+    console.log('Token will be deployed to', deployedAddress)
+
+    try {
+      await hre.run('verify:ButtonToken', { address: deployedAddress })
+    } catch (e) {
+      console.log('Unable to verify on etherscan', e)
+    }
+  })
+
+task('deploy:ChainlinkOracle')
+  .addParam('aggregator', 'the address of the backing chainlink aggregator')
+  .addParam('stalenessThresholdSecs', 'the number of seconds before refresh')
+  .setAction(async function (args: TaskArguments, hre) {
+      const { aggregator, stalenessThresholdSecs } = args
+      const ChainlinkOracle = await hre.ethers.getContractFactory('ChainlinkOracle')
+      const oracle = await ChainlinkOracle.deploy(aggregator, stalenessThresholdSecs)
+      await oracle.deployed()
+      console.log(`Oracle deployed to ${oracle.address}`)
+      return oracle.address;
+  })
--- tasks/deployers/index.ts
+++ tasks/deployers/index.ts
@@ -1,1 +1,2 @@
 import './unbutton'
+import './button'

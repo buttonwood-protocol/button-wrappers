bd28e16589ef3c8d918191ff80c654ba11836f61
SocksNFlops <socks&flops@prl.one>
2022-10-28 14:09:10 -0700

    adding button-token-router deployer-scripts

---
+++ tasks/deployers/ButtonTokenWamplRouter.ts
@@ -0,0 +1,35 @@
+import { task, types } from 'hardhat/config'
+import { TaskArguments } from 'hardhat/types'
+
+task('deploy:ButtonTokenWamplRouter')
+  .addParam('wampl', 'the address of wampl', undefined, types.string, false)
+  .setAction(async function(
+    _args: TaskArguments,
+    hre,
+  ) {
+    const { wampl } = _args;
+    console.log('Signer', await (await hre.ethers.getSigners())[0].getAddress());
+    const ButtonTokenWamplRouter = await hre.ethers.getContractFactory('ButtonTokenWamplRouter');
+    const buttonTokenWamplRouter = await ButtonTokenWamplRouter.deploy(wampl);
+    await buttonTokenWamplRouter.deployed();
+    const buttonTokenWamplRouterAddress = buttonTokenWamplRouter.address;
+    console.log(`ButtonTokenWamplRouter deployed to ${buttonTokenWamplRouterAddress}`);
+
+    try {
+      await hre.run('verify:ButtonTokenWamplRouter', { address: buttonTokenWamplRouterAddress, wampl });
+    } catch (e) {
+      console.log('Unable to verify on etherscan', e);
+    }
+});
+
+task('verify:ButtonTokenWamplRouter', 'Verifies on etherscan')
+  .addParam('address', 'the contract address', undefined, types.string, false)
+  .addParam('wampl', 'the address of wampl', undefined, types.string, false)
+  .setAction(async function(args: TaskArguments, hre) {
+    const { address, wampl } = args
+
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments: [wampl],
+    })
+});
---
+++ tasks/deployers/ButtonTokenWethRouter.ts
@@ -0,0 +1,35 @@
+import { task, types } from 'hardhat/config'
+import { TaskArguments } from 'hardhat/types'
+
+task('deploy:ButtonTokenWethRouter')
+  .addParam('weth', 'the address of weth', undefined, types.string, false)
+  .setAction(async function(
+    _args: TaskArguments,
+    hre,
+  ) {
+    const { weth } = _args;
+    console.log('Signer', await (await hre.ethers.getSigners())[0].getAddress());
+    const ButtonTokenWethRouter = await hre.ethers.getContractFactory('ButtonTokenWethRouter');
+    const buttonTokenWethRouter = await ButtonTokenWethRouter.deploy(weth);
+    await buttonTokenWethRouter.deployed();
+    const buttonTokenWethRouterAddress = buttonTokenWethRouter.address;
+    console.log(`ButtonTokenWethRouter deployed to ${buttonTokenWethRouterAddress}`);
+
+    try {
+      await hre.run('verify:ButtonTokenWethRouter', { address: buttonTokenWethRouterAddress, weth });
+    } catch (e) {
+      console.log('Unable to verify on etherscan', e);
+    }
+});
+
+task('verify:ButtonTokenWethRouter', 'Verifies on etherscan')
+  .addParam('address', 'the contract address', undefined, types.string, false)
+  .addParam('weth', 'the address of weth', undefined, types.string, false)
+  .setAction(async function(args: TaskArguments, hre) {
+    const { address, weth } = args
+
+    await hre.run('verify:verify', {
+      address,
+      constructorArguments: [weth],
+    })
+});

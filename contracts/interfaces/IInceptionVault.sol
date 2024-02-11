49ecdededba9261426d1e0723ae65fe0b1e6809c
SocksNFlops <socks&flops@prl.one>
2024-02-10 21:40:48 -0500

    feat(inception-token-oracle): creating inception token oracle

---
+++ contracts/interfaces/IInceptionVault.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-3.0
+
+// solhint-disable-next-line
+/// @dev https://github.com/inceptionlrt/smart-contracts/blob/master/contracts/interfaces/IInceptionVault.sol
+interface IInceptionVault {
+    function ratio() external view returns (uint256);
+}
